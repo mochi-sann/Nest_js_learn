eb4a3c3356eb2484fe5fab0b9157057611c8f61c
mochi-sann <moyurusuto@gmail.com>
2021-04-11 10:58:56 +0900

    色々した

--- src/docs/docs.controller.ts
+++ src/docs/docs.controller.ts
@@ -1,22 +1,51 @@
-import { Controller, Get, HttpCode, Param } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Query,
+  Redirect,
+} from "@nestjs/common";
 
-const GuusuuKisuu = (number: number): string => {
-  if (number % 2 === 0) {
-    return "偶数";
-  } else if (number % 2 === 1) {
-    return "奇数";
-  } else {
-    return "偶数でも奇数でもありません";
-  }
+const GuusuuKisuu = (number: number): number => {
+  return (
+    ((number * number * number) / 13.427 + Math.random()) *
+    (Math.random() * 600)
+  );
 };
 
 @Controller("docs")
 export class DocsController {
+  // @Post()
+  // create(@Body("name") createCatDto: any) {
+  //   return "This action adds a new cat" + JSON.stringify(createCatDto);
+  // }
+
+  @Post() // HTTP メソッドの指定
+  async create(@Body() createCatDto) {
+    // リクエストの Body を取得
+    // this.catsService.create(createCatDto); // 受け取った値を Service に渡す
+    return GuusuuKisuu(createCatDto.date);
+  }
+
   @Get()
   @HttpCode(404)
   findAll(): { body: string } {
     return { body: "none" };
   }
+
+  @Get("docs")
+  @Redirect("https://docs.nestjs.com", 302)
+  getDocs(@Query("version") version) {
+    console.log(version);
+
+    if (version && version === "5") {
+      return { url: "https://docs.nestjs.com/v5/" };
+    }
+  }
+
   @Get(":id")
   @HttpCode(200)
   findOne(@Param() params): string {
